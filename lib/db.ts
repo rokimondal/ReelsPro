1094bfd1cea63d84ac9aea791baacb2f00522567
ROKI MONDAL <rokimondal@ROKIs-MacBook-Air.local>
2025-02-02 00:08:47 +0530

    create models

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose'
 const MONGODB_URI = process.env.MONGODB_URI!;
 
+
 if (!MONGODB_URI) {
     throw new Error("Please define mongodb uri in env file");
 }
@@ -23,7 +24,9 @@ export async function connectToDatabase() {
         }
         cached.promise = mongoose
             .connect(MONGODB_URI, opts)
-            .then(() => mongoose.connection)
+            .then(() => {
+                return mongoose.connection
+            })
     }
     try {
         cached.conn = await cached.promise
---
+++ models/Video.ts
@@ -0,0 +1,45 @@
+import mongoose, { model, models, Schema } from 'mongoose'
+import bcrypt from 'bcryptjs'
+
+export const VIDEO_DIMENSIONS = {
+    width: 1080,
+    height: 1920
+}
+
+export interface IVideo {
+    _id?: string;
+    title: string;
+    description: string;
+    videoUrl: string;
+    thumbnailUrl?: string;
+    controls: boolean;
+    transformation: {
+        width: number;
+        height: number;
+        quality?: number;
+    };
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const videoSchema = new Schema<IVideo>(
+    {
+        title: { type: String, required: true },
+        description: { type: String, required: true },
+        videoUrl: { type: String, required: true },
+        thumbnailUrl: { type: String, required: true },
+        controls: { type: Boolean, default: true },
+        transformation: {
+            width: { type: Number, default: VIDEO_DIMENSIONS.width },
+            height: { type: Number, default: VIDEO_DIMENSIONS.height },
+            quality: { type: Number, min: 1, max: 100 }
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+const Video = models?.Video || model<IVideo>('Video', videoSchema);
+
+export default Video
